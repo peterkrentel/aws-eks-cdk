6fffee3912a1ed072b7cb4af1f18acc0bd39f0ae
Peter Krentel <peterkrentel@gmail.com>
2025-06-10 12:50:20 -0500

    removing kubectl_layer

--- aws_eks_cdk/aws_eks_cdk_stack.py
+++ aws_eks_cdk/aws_eks_cdk_stack.py
@@ -9,13 +9,9 @@ class AwsEksCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Create an EKS cluster
-        kubectl_layer = eks.KubectlLayer(self, "KubectlLayer")
-
         eks.Cluster(
             self, "MyEksCluster",
             version=eks.KubernetesVersion.V1_29,
             default_capacity=2,
-            kubectl_layer=kubectl_layer,
             default_capacity_instance=eks.InstanceType("t3.medium"),
         )
