6b5322b77a1b3599b02adbbe9e6dee51b9872172
Peter Krentel <peterkrentel@gmail.com>
2025-06-10 12:14:12 -0500

    kubectl layer

--- aws_eks_cdk/aws_eks_cdk_stack.py
+++ aws_eks_cdk/aws_eks_cdk_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack,
     aws_eks as eks,
+    aws_lambda as _lambda,
 )
 from constructs import Construct
 
@@ -8,8 +9,13 @@ class AwsEksCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Create an EKS cluster
+        kubectl_layer = eks.KubectlLayer(self, "KubectlLayer")
+
         eks.Cluster(
             self, "MyEksCluster",
             version=eks.KubernetesVersion.V1_29,
             default_capacity=2,
+            kubectl_layer=kubectl_layer,
+            default_capacity_instance=eks.InstanceType("t3.medium"),
         )
